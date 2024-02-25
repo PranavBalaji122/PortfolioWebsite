152c2773f3021610ae3ea86c5d0d73697f3aa702
PersonwhoPlayz <77422798+PersonwhoPlayz@users.noreply.github.com>
2024-02-25 18:24:43 -0500

    started working on aboutMe

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -3,10 +3,20 @@ import './AboutMe.css';
 
 function AboutMe(){
     return(
-        <div className="ContentWrapperAboutMe" id = "aboutMeSection">
-            <h1 className="AboutMeHeader">About Me!</h1>
-            <h1 className="tinyLineUnder"></h1>
+        <div className="AboutMeMain">
+           <div className="AboutMeHeaderDiv" id = "aboutMeSection">
+              <h1 className="AboutMeHeader">About Me!</h1>
+                <h1 className="TinyLineUnder"></h1>
+          </div>
+          <div className="GetToKnowMeSkillsGrid">
+            <div className="GetToKnowMMeDiv">
+            <h2>Get to Know Me!</h2> 
+            </div>
+            <div className="SkillsDiv">
+                <h2>Skills</h2>    
+            </div>
             
+          </div>
         </div>
     )
 }
