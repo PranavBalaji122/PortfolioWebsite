ea35abe9cdfa088751def7627715a8374974f367
PersonwhoPlayz <77422798+PersonwhoPlayz@users.noreply.github.com>
2024-02-25 00:49:54 -0500

    .

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -3,7 +3,7 @@ import './AboutMe.css';
 
 function AboutMe(){
     return(
-        <div className="ContentWrapper">
+        <div className="ContentWrapper" id = "aboutMe">
             <h1 className="AboutMeHeader">About Me!</h1>
 
         </div>
