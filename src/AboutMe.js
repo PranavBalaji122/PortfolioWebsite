0570a25f34c9f359e1c5ece9c6c5d753367270fb
PersonwhoPlayz <77422798+PersonwhoPlayz@users.noreply.github.com>
2024-02-25 22:28:18 -0500

    added stuff

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,10 +10,19 @@ function AboutMe(){
           </div>
           <div className="GetToKnowMeSkillsGrid">
             <div className="GetToKnowMMeDiv">
-            <h2>Get to Know Me!</h2> 
+              <h2>Get to Know Me!</h2> 
+              <div className="ParagraphDiv">
+               <p className="Paragraph1Div">Hey, I'm Pranav Balaji from Marlbrough, MA! I am currently a senior at the Advanced Math and Science Academy, intrested in pursuing a 
+                major in Computer Science. In particular, I am passionate about AI, Machine Learning, and Full Stack Development. </p>
+                <p>Other than computer scinece, some of my other passion/hobbies are:</p>
+                <ul>
+                  <li><text>FTC Robtics</text> : For the last 7 years, I have been part of First Tech Challange(FTC) Team 15089, the Circuit Breakers, where I actively design, build, and program robots to compete in various different enviorments!   </li>
+                </ul>
+              </div>
             </div>
             <div className="SkillsDiv">
                 <h2>Skills</h2>    
+                
             </div>
             
           </div>
